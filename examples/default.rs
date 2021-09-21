7031b2e205f1875927acf09bea46d7a48968c34d
Silene <51803887+AtropineTears@users.noreply.github.com>
2021-09-21 19:17:50 -0400

    Changed Values

--- examples/default.rs
+++ examples/default.rs
@@ -1,4 +1,4 @@
-use paranoid_hash::{ParanoidHash,OsAlgorithm};
+use paranoid_hash::{ParanoidHash};
 
 fn main(){
     // Default Config
@@ -7,6 +7,6 @@ fn main(){
     let context = ParanoidHash::default();
 
     // Blake2B and SHA512 Hash Function Returns
-    let (hash1,hash2) = context.read("example_file.txt");
+    let (_blake,_sha256) = context.read("example_file.txt");
 
 }
